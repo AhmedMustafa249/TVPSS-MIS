4d23474c67e2fbd2dc942237afdc553219f2e250
WaleedOthman2525 <alkhair20@graduate.utm.my>
2025-01-11 04:51:33 +0800

    Added Incomplete home Page

---
+++ src/main/java/com/example/tvpssmis4/controller/JPNJController.java
@@ -0,0 +1,24 @@
+package com.example.tvpssmis4.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/jpnj")
+public class JPNJController {
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model, HttpSession session) {
+        // Check if user is logged in and has correct role
+        if (session.getAttribute("role") == null ||
+                !session.getAttribute("role").equals("JPNJ")) {
+            return "redirect:/login";
+        }
+
+        model.addAttribute("username", session.getAttribute("username"));
+        return "/JPNJ/dashboard";
+    }
+}
